e030df345aa58eb468e90f377fa72658bc5cd0d6
Plinio Prado <plinio.prado@immaginare.com.br>
2017-10-12 16:20:23 -0700

    login state working

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,55 +1,34 @@
-import React, { Component } from 'react'
+import React from 'react'
 import './styles.css'
 
-class Login extends Component {
-  constructor() {
-    super()
-    this.state = {
-      email: 'john@example.com',
-      pass: '123456',
-      logged: false
-    }
-  }
-
-  handleEmailChange = (event) => {
-    this.setState({email: event.target.value})
-  }
-
-  handlePassChange = (event) => {
-    this.setState({pass: event.target.value})
-  }
-
-  handleSubmit = (event) => {
-    this.setState({logged: true})
-    event.preventDefault()
-  }
-  
-  render () {
-    return (
-      <form onSubmit={this.handleSubmit}>
-      <div className="loginForm">
-        <div>
-        <label>
-          Email:
-        <input type="text" value={this.state.email} onChange={this.handleEmailChange} />
-        </label>
-        </div>
-        <div>
-        <label>
-          Pass:
-        <input type="password" value={this.state.pass} onChange={this.handlePassChange} />
-        </label>
-        </div>
-        <div>
-        <input type="submit" value="Ok" />
-        </div>
-        <div>l:{this.state.logged.toString()}</div>
+const Login = ({
+  handleSubmit,
+  email,
+  pass,
+  logged,
+  handleEmailChange,
+  handlePassChange
+}) => (
+  <div className="loginForm">
+    <form onSubmit={handleSubmit}>
+      <div>
+      <label>
+        Email:
+      <input type="text" value={email} onChange={handleEmailChange} />
+      </label>
       </div>
-      </form>
-    )
-  }
-
-}
-
+      <div>
+      <label>
+        Pass:
+      <input type="password" value={pass} onChange={handlePassChange} />
+      </label>
+      </div>
+      <div>
+      <input type="submit" value="Ok" />
+      </div>
+      <div>l:{logged.toString()}</div>
+    </form>
+  </div>
+)
 
 export default Login
--- src/containers/Login/LoginContainer.js
+++ src/containers/Login/LoginContainer.js
@@ -1,3 +1,43 @@
+import React, { Component } from 'react'
 import Login from '../../components/Login'
 
-export default Login
+
+class LoginContainer extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      email: 'john@example.com',
+      pass: '123456',
+      logged: false
+    }
+  }
+
+  handleSubmit(e) {
+    console.log('data', this.state)
+    this.setState({logged: true})
+    e.preventDefault()
+  }
+
+  handleEmailChange = (e) => {
+    console.log('handleEmailChange')
+    this.setState({email: e.target.value})
+  }
+
+  handlePassChange = (e) => {
+    console.log('handlePassChange')
+    this.setState({pass: e.target.value})
+  }
+
+  render() {
+    return <Login
+      handleSubmit={e => this.handleSubmit(e)}
+      email={this.state.email}
+      pass={this.state.pass}
+      logged={this.state.logged}
+      handleEmailChange={e => this.handleEmailChange(e)}
+      handlePassChange={e => this.handlePassChange(e)}
+    />
+  }
+}
+
+export default LoginContainer
